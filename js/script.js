bb26ad6ad5c46107a2c414984b8052e8e6320ecd
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-09-04 15:37:19 -0700

    logic for show subject choice

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
 const choices = document.getElementById("subjects-form");
+const subjectHeader = document.querySelector(".subject-heading-container");
 
+const subjects = {
+  js: "Javascript",
+  html: "HTML",
+  css: "CSS",
+  accessibility: "Accessibility",
+};
 choices.addEventListener(
   "click",
   (e) => {
     e.preventDefault();
-    const val = e.target.closest("label").dataset.value;
-    console.log(val);
+    const val = e.target.closest("label")?.dataset.value;
+
+    if (val != undefined) {
+      //   console.log(val);
+      const img = subjectHeader.children[0];
+      img.setAttribute("src", `assets/images/icon-${val}.svg`);
+      img.setAttribute("alt", val);
+      img.classList.toggle("hide", false);
+
+      const headerText = subjectHeader.children[1];
+      headerText.textContent = subjects[val];
+    }
   },
   true
 );
