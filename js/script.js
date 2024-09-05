cf65d1f0abab9e3cbca906d5ddc98a8a87241a0f
mrcordova <46950394+mrcordova@users.noreply.github.com>
2024-09-04 20:51:30 -0700

    logic to show feedback to choice chosen

--- js/script.js
+++ js/script.js
@@ -11,13 +11,71 @@ for (const quiz of quizzes) {
   quizzesObj[quiz["title"]] = quiz;
 }
 console.log(quizzesObj);
+let questionCount = 4;
+let userChoiceLabel;
 const subjects = {
   js: "Javascript",
   html: "HTML",
   css: "CSS",
   accessibility: "Accessibility",
 };
-let questionCount = 1;
+
+function checkAnswer(e) {
+  //   e.preventDefault();
+  //   console.log(e.target.tagName);
+  const choiceLabel = e.target.closest("label");
+  const eleTagName = e.target.tagName;
+  if (choiceLabel) {
+    const choiceInput = choiceLabel.querySelector("input[type='radio'");
+    choiceInput.checked = true;
+    userChoiceLabel = choiceLabel;
+  } else if (eleTagName === "BUTTON" && userChoiceLabel == undefined) {
+    e.preventDefault();
+    // console.log("no choice");
+    document.getElementById("no-answer").classList.toggle("hide", false);
+  } else if (eleTagName === "BUTTON") {
+    e.preventDefault();
+
+    const answer =
+      quizzesObj[subjectHeader.dataset.subject].questions[questionCount].answer;
+    const answerLabel = document.querySelector(
+      `label[data-choice="${answer}"]`
+    );
+    // console.log(userChoiceLabel);
+    const userChoice = userChoiceLabel.dataset.choice;
+    if (answer === userChoice) {
+      userChoiceLabel.insertAdjacentHTML(
+        "beforeend",
+        `<img src='/assets/images/icon-correct.svg' alt='checkmark'/>`
+      );
+      userChoiceLabel.classList.add("correct");
+    } else if (answer !== userChoice) {
+      userChoiceLabel.insertAdjacentHTML(
+        "beforeend",
+        `<img src='/assets/images/icon-incorrect.svg' alt='x'/>`
+      );
+      answerLabel.insertAdjacentHTML(
+        "beforeend",
+        `<img class="correct-img" src='/assets/images/icon-correct.svg' alt='checkmark'/>`
+      );
+      userChoiceLabel.classList.add("incorrect");
+
+      for (const formEle of userChoiceLabel.parentElement.children) {
+        formEle.disabled = true;
+        formEle.style = "pointer-events: none;";
+      }
+      e.target.textContent = "Next Question";
+      e.target.disabled = false;
+      e.target.style = "";
+
+      // Might not neeeded since we should move to the next page
+      if (!e.target.nextElementSibling.classList.contains("hide")) {
+        e.target.nextElementSibling.classList.toggle("hide", true);
+      }
+    }
+  }
+}
+
 choices.addEventListener(
   "click",
   (e) => {
@@ -32,9 +90,12 @@ choices.addEventListener(
 
       const headerText = subjectHeader.children[1];
       headerText.textContent = subjects[val];
+      subjectHeader.dataset.subject = subjects[val];
 
       // Question Template
       const clone = questionTemplate.content.cloneNode(true);
+      const choiceForm = clone.getElementById("choice-form");
+      choiceForm.addEventListener("click", checkAnswer, true);
       contentArea.replaceChildren(clone);
     }
   },
